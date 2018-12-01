db3edac8a977ec245427115daec9f05bd997e9b8
AGARWAL Vishal TGI/OLS <tfpw1884@orange.com>
2018-12-01 23:19:19 +0530

    Adding java application first file to git

---
+++ src/main/java/clinic/programming/training/Application.java
@@ -0,0 +1,14 @@
+package clinic.programming.training ;
+
+public class Application{
+	
+	public Application(){
+		System.out.println("Initializing application");
+	}
+	
+	public static void main(String[] args){
+		System.out.println("Starting application");
+		Application app = new Application();
+	}
+
+}
