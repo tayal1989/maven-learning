98ed839b0cc30c85f5b941e6a2097db9366e338d
AGARWAL Vishal TGI/OLS <tfpw1884@orange.com>
2018-12-01 23:48:12 +0530

    Changing java compiler to 1.8

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,6 +1,18 @@
 package clinic.programming.training ;
+import java.util.ArrayList ;
+import java.util.List ;
 
 public class Application{
+	
+	public void greet(){
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hello");
+		
+		for (String greeting : greetings) {
+			System.out.println("Greeting : " + greeting) ;
+		}
+	
+	}
 	
 	public Application(){
 		System.out.println("Initializing application");
@@ -9,6 +21,7 @@ public class Application{
 	public static void main(String[] args){
 		System.out.println("Starting application");
 		Application app = new Application();
+		app.greet();
 	}
 
 }
