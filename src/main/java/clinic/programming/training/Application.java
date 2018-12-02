56ca3743fc2e6c2e6a7d954cf0225f5c6a1a521f
AGARWAL Vishal TGI/OLS <tfpw1884@orange.com>
2018-12-02 13:48:22 +0530

    Used string utils and added dependency in pom.xml file

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,8 +1,14 @@
 package clinic.programming.training ;
 import java.util.ArrayList ;
 import java.util.List ;
+import org.apache.commons.lang3.StringUtils ;
 
 public class Application{
+	
+	public int countWords(String words) {
+			String[] separateWords = StringUtils.split(words, ' ') ;
+			return (separateWords == null) ? 0 : separateWords.length ;
+	}
 	
 	public void greet(){
 		List<String> greetings = new ArrayList<>();
@@ -22,6 +28,9 @@ public class Application{
 		System.out.println("Starting application");
 		Application app = new Application();
 		app.greet();
+		
+		int count = app.countWords("I have four words") ;
+		System.out.println("Word count : " + count) ;
 	}
 
 }
